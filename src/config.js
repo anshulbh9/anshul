73f986dd92bf859a4873b7d1f288948e70c272c2
anshulbh9 <anshulbh9@gmail.com>
2023-12-11 20:37:40 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,27 +1,19 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'anshulbh9@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/anshulbh9',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/anshulbh9',
     },
     {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+      name: 'LeetCode',
+      url: 'https://leetcode.com/anshulbh9/',
+    }
   ],
 
   navLinks: [
