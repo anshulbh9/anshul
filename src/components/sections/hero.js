5ab927fbd3764e5b10b1b403cb88b81840fdcdb3
anshulbh9 <anshulbh9@gmail.com>
2023-12-11 20:20:49 +0530

    change name

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Anshul Bhardwaj.</h2>;
+  const three = <h3 className="big-heading">I am a backend and distributed systems engineer.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I’m a software engineer specializing in building exceptional
+        digital experiences. Currently, I’m working as member of technical staff
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+          Salesforce
         </a>
         .
       </p>
